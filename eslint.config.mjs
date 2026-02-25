7a711f86a1dfd172346cf938207cb052064df6cb
adedamolabasit <adedamolabasit09@gmail.com>
2026-02-25 12:01:15 +0100

    chore: clean up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,36 @@
-// import nextConfig from "eslint-config-next";
-// import tseslint from "@typescript-eslint/eslint-plugin";
-// import tsParser from "@typescript-eslint/parser";
-// import unusedImports from "eslint-plugin-unused-imports";
+import nextConfig from "eslint-config-next";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import unusedImports from "eslint-plugin-unused-imports";
 
-// export default [
-//   ...nextConfig, 
+export default [
+  ...nextConfig, 
 
-//   {
-//     files: ["**/*.ts", "**/*.tsx"],
-//     languageOptions: {
-//       parser: tsParser,
-//     },
-//     plugins: {
-//       "@typescript-eslint": tseslint,
-//       "unused-imports": unusedImports,
-//     },
-//     // rules: {
-//     //   "no-console": "error",
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "no-console": "error",
 
-//     //   "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-explicit-any": "error",
 
-//     //   "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-imports": "error",
 
-//     //   "unused-imports/no-unused-vars": [
-//     //     "error",
-//     //     {
-//     //       vars: "all",
-//     //       varsIgnorePattern: "^_",
-//     //       args: "after-used",
-//     //       argsIgnorePattern: "^_",
-//     //     },
-//     //   ],
-//     // },
-//   },
-// ];
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
