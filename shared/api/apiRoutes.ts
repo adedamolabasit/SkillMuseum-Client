e0482e3974dad735ed61566fbbc6c4c9f0024867
adedamolabasit <adedamolabasit09@gmail.com>
2026-03-17 14:05:54 +0100

    feat: updates

--- shared/api/apiRoutes.ts
+++ shared/api/apiRoutes.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://skillmuseum-server-production.up.railway.app/api";
+export const API_BASE_URL = "https://adaptable-creativity-production-aeee.up.railway.app/api";
 export const VERIFY_TOKEN = `${API_BASE_URL}/auth/verify`;
 export const LOGOUT_USER = `${API_BASE_URL}/auth/logout`;
 export const PROFILE = `${API_BASE_URL}/auth/me`;
